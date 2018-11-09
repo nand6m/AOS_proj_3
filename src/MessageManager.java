313e5260f7e75b749ec4ac3999aea21e6df0c6a4
Nandish <13392242+nand6m@users.noreply.github.com>
2018-11-08 19:57:05 -0600

    Minor changes to MessageManager

--- src/MessageManager.java
+++ src/MessageManager.java
@@ -28,9 +28,6 @@ public class MessageManager extends Thread implements MsgListener, Sender{
 //		broadcast = b;
 //	}
 
-//	static void setRCMTester(MutexTest)
-//	{
-//	}
 
 	static void setRCMutex(RCMutex rcminput)
 	{
